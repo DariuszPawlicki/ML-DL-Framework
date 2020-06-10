Correcting backpropagation errors.

NanoFlow/decorators.py
@@ -1,4 +1,4 @@
-import numpy as np
+from numpy import ndarray, array
 
 
 def to_numpy_array(func):
@@ -7,16 +7,16 @@ def to_numpy_array(func):
         args = list(args)
 
         for i, arg in enumerate(args):
-            if not (isinstance(arg, np.ndarray)) and \
+            if not (isinstance(arg, ndarray)) and \
                     (isinstance(arg, list) or isinstance(arg, tuple)):
 
-                args[i] = np.array(arg)
+                args[i] = array(arg)
 
         for key, kwarg in kwargs.items():
-            if not isinstance(kwarg, np.ndarray) and \
+            if not isinstance(kwarg, ndarray) and \
                     (isinstance(kwarg, list) or isinstance(kwarg, tuple)):
 
-                kwargs[key] = np.array(kwarg)
+                kwargs[key] = array(kwarg)
 
         return func(*args, **kwargs)
 
@@ -28,17 +28,17 @@ def add_second_dim(func):
         args = list(args)
 
         for i, arg in enumerate(args):
-            if isinstance(arg, np.ndarray):
+            if isinstance(arg, ndarray):
 
                 try:
                     assert arg.shape[1]
                 except(IndexError):
                     arg = arg.reshape(arg.shape[0], 1)
 
-                args[i] = np.array(arg)
+                args[i] = array(arg)
 
         for key, kwarg in kwargs.items():
-            if isinstance(kwarg, np.ndarray):
+            if isinstance(kwarg, ndarray):
                 try:
                     assert kwarg.shape[1]
                 except(IndexError):

NanoFlow/deep_learning/neural_net.py
@@ -1,9 +1,8 @@
 from deep_learning.layers.layers import *
-from utils.activations import *
-from utils.cost_functions import *
 from utils.data_processing import *
 from utils.metrics import *
 from decorators import add_second_dim
+from numpy import sqrt, random, ones, dot, zeros, sum, array, argmax
 
 
 class NeuralNet:
@@ -14,6 +13,7 @@ class NeuralNet:
         self.biases = None
         self.cost = None
         self.metrics = None
+        self.trainable_layers = None
 
 
     def add_layer(self, layer: Layer):
@@ -33,6 +33,7 @@ class NeuralNet:
                 raise TypeError("Last layer of model must be an"
                                 " 'OutputLayer' type.")
 
+            self.trainable_layers = sum([1 for layer in self.layers if layer.trainable])
             self.weights_init(param_init = param_init)
             self.metrics = pick_metrics_method(metrics)
 
@@ -40,19 +41,23 @@ class NeuralNet:
     def weights_init(self, param_init: str):
         weights = []
 
-        for i in range(len(self.layers)):
+        for cur_layer_index, current_layer in enumerate(self.layers):
 
-            if param_init == "xavier":
-                method = np.sqrt(1 / self.layers[i].input_shape)
-            elif param_init == "he_normal":
-                method = np.sqrt(2 / (self.layers[i - 1].input_shape + self.layers[i].input_shape))
-            else:
-                method = 1
+            if current_layer.trainable == True:
 
-            weights.append(np.random.randn(self.layers[i].input_shape, self.layers[i].output) * method)
+                if param_init == "xavier":
+                    method = sqrt(1 / current_layer.input_shape)
+                elif param_init == "he_normal":
+                    method = sqrt(2 / (self.layers[cur_layer_index - 1].input_shape +
+                                       current_layer.input_shape))
+                else:
+                    method = 1
 
-        self.weights = np.array(weights)
-        self.biases = np.ones((len(self.layers), 1))
+                weights.append(random.randn(current_layer.input_shape, current_layer.output) * method)
+
+        self.weights = array(weights)
+
+        self.biases = ones((self.trainable_layers, 1))
 
 
     def forward_propagation(self, X):
@@ -61,18 +66,22 @@ class NeuralNet:
 
         z = X
 
+        non_trainable_activated = 0
+
         z_cache = []
         a_cache = []
 
         a_cache.append(X)
 
-        for i in range(len(self.layers)):
-            current_layer = self.layers[i]
+        for cur_layer_index, current_layer in enumerate(self.layers):
+            cur_layer_index -= non_trainable_activated
 
-            if i == len(self.layers) - 1:
-                a = current_layer.activate(np.dot(z, self.weights[i]) + self.biases[i])
+            if current_layer.trainable:
+                a = current_layer.activate(dot(z, self.weights[cur_layer_index]) +
+                                           self.biases[cur_layer_index])
             else:
-                a = current_layer.activate(np.dot(z, self.weights[i]) + self.biases[i])
+                a = current_layer.activate(z)
+                non_trainable_activated += 1
 
             a_cache.append(a)
             z_cache.append(z)
@@ -88,53 +97,58 @@ class NeuralNet:
                                          derivative = True)
 
         delta_W = []
-        delta_b = np.zeros((self.biases.shape[0], 1))
+        delta_b = zeros((self.biases.shape[0], 1))
 
-        delta_b[-1] += np.sum(output_error)
+        delta_b[-1] += sum(output_error)
 
-        for layer in range(len(self.layers)):
-            delta_W.append(np.zeros((self.layers[layer].input_shape, self.layers[layer].output)))
+        for current_layer in self.layers:
+            if current_layer.trainable == True:
+                delta_W.append(zeros((current_layer.input_shape,
+                                      current_layer.output)))
 
-        for i in range(len(self.layers) - 1, -1, -1): # Loop for deriving cost function
-                                                      # with respect to weights[current_layer]
+        for cur_layer_index, current_layer in reversed(list(enumerate(self.layers))): # Loop for deriving cost function
+                                                                                      # with respect to weights[current_layer]
 
-            current_layer = self.layers[i]
+            if current_layer.trainable == False or \
+                    current_layer.__name__ == OutputLayer.__name__:
 
-            if current_layer.__name__ == BatchNormalization.__name__ or \
-                            current_layer.__name__ == OutputLayer.__name__:
                 continue
 
             layer_gradient = output_error
 
-            derived_layer = len(self.layers) - 1
 
-            while derived_layer >= i:
-                if derived_layer == i:
-                    delta_b[i] += np.sum(layer_gradient)
+            for derived_index, derived_layer in reversed(list(enumerate(self.layers))):
+                if derived_layer.trainable == False:
+                    continue
 
-                    layer_gradient = np.dot(layer_gradient.T, a_cache[i])
+                if derived_index == cur_layer_index:
+                    delta_b[cur_layer_index] += sum(layer_gradient)
 
-                    delta_W[i] += layer_gradient.T
+                    layer_gradient = dot(layer_gradient.T, a_cache[cur_layer_index])
+
+                    delta_W[cur_layer_index] += layer_gradient.T
 
                     break
+
                 else:
-                    layer_gradient = np.dot(layer_gradient, self.weights[derived_layer].T)
-                    layer_gradient *= current_layer.activate(z_cache[derived_layer], derivative = True)
+                    layer_gradient = dot(layer_gradient, self.weights[derived_index].T)
+                    layer_gradient *= current_layer.activate(z_cache[derived_index], derivative = True)
 
-                    derived_layer -= 1
-
-        delta_W = np.array(delta_W)
+        delta_W = array(delta_W)
 
         return delta_W, delta_b
 
 
     @to_numpy_array
     @add_second_dim
-    def train(self, X, y_labels, epochs = 100,
+    def train(self, X, y_labels, epochs = 10000,
               learning_rate = 0.01, batch_size = 100,
-              verbose = True):
+              verbose = True, patience = 10):
 
-        for i in range(epochs):
+        previous_cost = 0
+        patience_counter = patience
+
+        for epoch in range(epochs):
 
             a_cache, z_cache = self.forward_propagation(X)
 
@@ -144,18 +158,37 @@ class NeuralNet:
             db /= len(X)
 
             for layer, grad in enumerate(dW):
-                self.weights[layer] -= grad
+                if self.layers[layer].trainable == True:
+                    self.weights[layer] -= grad
 
             self.biases -= db
 
             if verbose == True:
                 cost = self.layers[-1].cost(y_labels, a_cache[-1])
-                print("Cost in epoch {} :".format(i + 1), cost)
+
+                if cost >= previous_cost:
+                    patience_counter -= 1
+
+                    if patience_counter == 0:
+                        print("Gradient descent is on plateau/diverging,"
+                              "\ntry different learning rate or "
+                              "feature engineering.\n")
+
+                        print("Cost: ", cost)
+
+                        break
+
+                else:
+                    patience_counter = patience
+
+                previous_cost = cost
+
+                print("Cost in epoch {} :".format(epoch + 1), cost)
 
 
     def predict(self, data):
         a_cache, _ = self.forward_propagation(data)
-        return np.argmax(a_cache[-1], axis = 1)
+        return argmax(a_cache[-1], axis = 1)
 
 
     def evaluate(self, y_labels, predictions):
@@ -166,10 +199,10 @@ class NeuralNet:
 if __name__ == '__main__':
     net = NeuralNet()
 
-    net.add_layer(InputLayer(2, 100, activation = "relu"))
-    net.add_layer(BatchNormalization(100))
-    net.add_layer(DenseLayer(100, activation = "relu"))
-    net.add_layer(DenseLayer(100, activation = "relu"))
+    net.add_layer(InputLayer(2, 1000, activation = "relu"))
+    net.add_layer(Dropout(1000, rate = 0.2))
+    net.add_layer(DenseLayer(500, activation = "relu"))
+    net.add_layer(DenseLayer(900, activation = "relu"))
     net.add_layer(OutputLayer(2, activation = "softmax",
                               cost_function = "categorical_crossentropy"))
 

NanoFlow/machine_learning/linear_models.py
@@ -1,4 +1,4 @@
-import numpy as np
+from numpy import dot, random
 from utils.cost_functions import mean_squared_error
 from decorators import to_numpy_array, add_second_dim
 from utils.metrics import pick_metrics_method
@@ -37,7 +37,7 @@ class LinearRegressor(LinearModel):
         super().__init__()
 
     def weights_init(self):
-        self.params = np.random.randn(1, self.features + 1)
+        self.params = random.randn(1, self.features + 1)
 
     @to_numpy_array
     @add_second_dim
@@ -89,7 +89,7 @@ class LinearRegressor(LinearModel):
         if X.shape[0] != self.features:
             X = X.T
 
-        return np.dot(self.params[0][1:], X) + self.params[0][0]
+        return dot(self.params[0][1:], X) + self.params[0][0]
 
     @to_numpy_array
     @add_second_dim
@@ -108,7 +108,7 @@ class LogisticRegressor(LinearModel):
 
 
     def weights_init(self):
-        self.params = np.random.randn(1, self.features + 1)
+        self.params = random.randn(1, self.features + 1)
 
     @to_numpy_array
     @add_second_dim

NanoFlow/utils/activations.py
@@ -1,4 +1,4 @@
-import numpy as np
+from numpy import exp, maximum, sum, var, mean
 
 
 def pick_activation(activation: str):
@@ -19,22 +19,22 @@ def relu(X, derivative = False):
 
         return X
 
-    return np.maximum(0, X)
+    return maximum(0, X)
 
 
 def sigmoid(X, derivative = False):
     if derivative == True:
         return sigmoid(X) * (1 - sigmoid(X))
 
-    return 1 / (1 + np.exp(-X))
+    return 1 / (1 + exp(-X))
 
 
 def softmax(X):
-    return np.exp(X) / np.sum(np.exp(X), axis = 1, keepdims = True)
+    return exp(X) / sum(exp(X), axis = 1, keepdims = True)
 
 
 def normalization(X):
-    mean = np.mean(X)
-    variance = np.var(X)
+    me = mean(X)
+    variance = var(X)
 
-    return (X - mean) / variance
+    return (X - me) / variance

NanoFlow/utils/cost_functions.py
@@ -1,4 +1,4 @@
-import numpy as np
+from numpy import sum, log
 from decorators import add_second_dim
 
 
@@ -24,7 +24,7 @@ def categorical_crossentropy(y_labels, output, derivative = False):
     if derivative == True:  # Derivative of softmax crossentropy function with respect to weights
         return output - y_labels
 
-    return -np.sum(y_labels * np.log(output)) / len(output)
+    return -sum(y_labels * log(output)) / len(output)
 
 
 def binary_crossentropy(y_labels, output, derivative = False):
@@ -32,17 +32,17 @@ def binary_crossentropy(y_labels, output, derivative = False):
         return output - y_labels
 
     if y_labels[0] == 1:
-        return -np.sum(np.log(1 - output)) / len(output)
+        return -sum(log(1 - output)) / len(output)
     elif y_labels[1] == 1:
-        return -np.sum(np.log(output)) / len(output)
+        return -sum(log(output)) / len(output)
 
 
 @add_second_dim
 def mean_squared_error(target, predictions, X = None, derivative = False):
     if derivative == True:
-        dW = np.sum((predictions - target) * X.T) / len(predictions)
-        db = np.sum((predictions - target)) / len(predictions)
+        dW = sum((predictions - target) * X.T) / len(predictions)
+        db = sum((predictions - target)) / len(predictions)
 
         return dW, db
 
-    return np.sum((predictions - target) ** 2) / len(predictions)
+    return sum((predictions - target) ** 2) / len(predictions)

NanoFlow/utils/data_processing.py
@@ -1,17 +1,17 @@
-import numpy as np
+from numpy import unique, zeros, sort, ndarray, argmax, array, random
 from decorators import to_numpy_array, add_second_dim
 
 
 @to_numpy_array
-def one_hot_encoder(target_labels: np.ndarray):
+def one_hot_encoder(target_labels: ndarray):
 
-    classes = np.unique(target_labels, return_counts = True)
+    classes = unique(target_labels, return_counts = True)
 
-    one_hot_labels = np.zeros((target_labels.shape[0], len(classes[1])))
+    one_hot_labels = zeros((target_labels.shape[0], len(classes[1])))
 
     new_ids = {}
 
-    for i, cl in enumerate(np.sort(classes[0])):
+    for i, cl in enumerate(sort(classes[0])):
         """
         Changing classes numeration, e.g. if classes in 'target_labels'
         aren't continuous - [0, 1, 3] - this loop will transform them to [0, 1, 2].
@@ -24,11 +24,11 @@ def one_hot_encoder(target_labels: np.ndarray):
     for index, label in enumerate(new_encoding):
         one_hot_labels[index][label] = 1
 
-    return np.array(one_hot_labels)
+    return array(one_hot_labels)
 
 
 @to_numpy_array
-def decode_one_hot(labels: np.ndarray):
+def decode_one_hot(labels: ndarray):
 
     try:
         assert labels.shape[1] >= 2
@@ -36,16 +36,16 @@ def decode_one_hot(labels: np.ndarray):
         classes = []
 
         for row in labels:
-            classes.append(np.argmax(row))
+            classes.append(argmax(row))
 
-        return np.array(classes)
+        return array(classes)
 
     except(AssertionError):
         return labels
 
 
 @to_numpy_array
-def data_split(data: np.ndarray, labels: np.ndarray, validation_split = False,
+def data_split(data: ndarray, labels: ndarray, validation_split = False,
                shuffle_data = True, split_size = 0.2):
 
     """
@@ -56,7 +56,7 @@ def data_split(data: np.ndarray, labels: np.ndarray, validation_split = False,
                         "labels has length {}.".format(len(data), len(labels)))
 
     if shuffle_data == True:
-        shuffler = np.random.permutation(len(labels))
+        shuffler = random.permutation(len(labels))
 
         data = data[shuffler]
         labels = labels[shuffler]

NanoFlow/utils/metrics.py
@@ -1,4 +1,4 @@
-import numpy as np
+from numpy import sum, ndarray, var
 from utils.data_processing import decode_one_hot
 from decorators import to_numpy_array, add_second_dim
 from utils.cost_functions import mean_squared_error
@@ -13,20 +13,20 @@ def pick_metrics_method(method: str):
 
 @to_numpy_array
 @add_second_dim
-def accuracy(target_labels: np.ndarray, predictions: np.ndarray):
+def accuracy(target_labels: ndarray, predictions: ndarray):
     assert len(target_labels) == len(predictions), ("Incompatibile shapes, target_labels has length {}, "
                         "predictions has length {}.".format(len(target_labels), len(predictions)))
 
     predictions = decode_one_hot(predictions)
     target_labels = decode_one_hot(target_labels)
 
-    return str((np.sum((target_labels == predictions)) / len(target_labels)) * 100) + " %"
+    return str((sum((target_labels == predictions)) / len(target_labels)) * 100) + " %"
 
 
 @to_numpy_array
 @add_second_dim
 def r_squared(Y, predictions):
-    variance = np.var(Y)
+    variance = var(Y)
     mse = mean_squared_error(Y, predictions)
 
     return 1 - (variance / mse)