a99e2e73272dd8c641a6078f70b53a2519471188
Darek <darekpl9@gmail.com>
2020-06-29 20:48:17 +0200

    .gitignore fix, added all tests for activation functions.

--- NanoFlow/tests/test_activations.py
+++ NanoFlow/tests/test_activations.py
@@ -1,4 +1,4 @@
-from scipy.stats import logistic
+from scipy.special import expit, softmax as scipy_softmax
 from utils.activations import *
 import numpy as np
 import unittest
@@ -24,6 +24,17 @@ class TestActivations(unittest.TestCase):
 
         self.assertEqual(np.allclose(derivatives, tmp), True)
 
+    def test_sigmoid(self):
+        correct_activations = expit(self.test_data)
+        correct_derivatives = expit(self.test_data) * (1 - expit(self.test_data))
+
+        self.assertTrue(np.allclose(correct_activations, sigmoid(self.test_data, False)))
+        self.assertTrue(np.allclose(correct_derivatives, sigmoid(self.test_data, True)))
+
+    def test_softmax(self):
+        correct_activations = scipy_softmax(self.test_data, axis = 1)
+
+        self.assertTrue(np.allclose(correct_activations, softmax(self.test_data)))
 
 
 if __name__ == '__main__':
